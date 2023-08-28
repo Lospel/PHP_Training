70686efd4388b723d48efc4e80416f5c5401b7e4
KimDowi <rlaehdnl@gmail.com>
2023-08-28 23:11:24 +0900

    first commit

---
+++ data/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // echo : 리눅스 명령어로, 다음 내용을 문자열로 출력하라는 뜻
+        echo date('Y-m-d H:i:s');
+    ?>
+</body>
+</html>
