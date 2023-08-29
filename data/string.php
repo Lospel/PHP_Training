16b33a13d7e5aaafd97223583c2c67643bb600ef
KimDowi <rlaehdnl@gmail.com>
2023-08-29 23:51:41 +0900

    2023-08-29

---
+++ data/string.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>String & String Operator</h1>
+    <?php
+    echo "Hello \"w\"ord";
+    ?>
+    <!-- 결합 연산자 . -->
+    <h2>Concatenation operator</h2>
+    <?php
+    echo "Hello "."world";
+    ?>
+    <h2>String length function</h2>
+    <?php
+    echo strlen("Hello world");
+    ?>
+</body>
+</html>
