bdc673afb58d0c2fde1981b4176ca4b59b4d3862
KimDowi <rlaehdnl@gmail.com>
2023-08-30 23:11:09 +0900

    2023-08-30

---
+++ data/variable.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Variable</h1>
+    <!-- <?php
+    echo 1+1;
+    $a = 10;
+    echo $a + 1;
+    ?> -->
+    <?php
+    $name = "leezche";
+    echo "Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+    ".$name."
+    Atque odit harum nulla quis mollitia hic eaque eos animi 
+    ".$name."
+    porro beatae laborum temporibus rerum vel consectetur 
+    egoing
+    voluptates blanditiis, aut amet dignissimos! 
+    by  ".$name;
+    ?>
+</body>
+</html>
