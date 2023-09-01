87aed79f669918d57900cc322bb6555533637421
KimDowi <rlaehdnl@gmail.com>
2023-09-01 22:35:30 +0900

    2023-09-01

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php
+            echo $_GET['id'] ?? NULL;
+        ?>
+    </h2>
     <?php
-        // echo : 리눅스 명령어로, 다음 내용을 문자열로 출력하라는 뜻
-        echo date('Y-m-d H:i:s');
+    echo @file_get_contents("data/".$_GET['id']);
     ?>
 </body>
 </html>
